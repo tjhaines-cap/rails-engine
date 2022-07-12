40624212e8be9f437f4c8be06a34f668a0f0f98d
tjhaines-cap <tjhaines@eckerd.edu>
2022-07-11 20:17:18 -0600

    Feat: create item serializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::ItemsController < ApplicationController
 
   def index
-    render json: Item.all
+    render json: ItemSerializer.new(Item.all)
+    # render json: (Item.all)
   end
 
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name { Faker::Name.name }
     description { Faker::Lorem.paragraph }
     unit_price { Faker::Number.decimal(l_digits: 3, r_digits: 2) }
-    # merchant_id { factory.create(app/user::merchant.id) }
-    # association :merchant
+    # merchant_id { Faker::Number.number }
+    association :merchant
   end
 end
